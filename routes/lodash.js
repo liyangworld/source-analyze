9c4b611e46e7e473af3125b195c9420d4e12dc63
liyangworld <1909457483@qq.com>
2018-02-01 15:59:21 +0800

    lodash基础模块框架构建完成
    lodash基础模块框架构建完成

--- routes/lodash.js
+++ routes/lodash.js
@@ -10,7 +10,42 @@ router.get('/array', function(req, res, next) {
   res.render('lodash/array');
 });
 
+router.get('/collection', function(req, res, next) {
+  res.render('lodash/collection');
+});
 
-
-
+router.get('/date', function(req, res, next) {
+  res.render('lodash/date');
+});
+         
+router.get('/function', function(req, res, next) {
+  res.render('lodash/function');
+});
+router.get('/lang', function(req, res, next) {
+  res.render('lodash/lang');
+});
+router.get('/math', function(req, res, next) {
+  res.render('lodash/math');
+});
+router.get('/number', function(req, res, next) {
+  res.render('lodash/number');
+});
+router.get('/object', function(req, res, next) {
+  res.render('lodash/object');
+});
+router.get('/seq', function(req, res, next) {
+  res.render('lodash/seq');
+});
+router.get('/string', function(req, res, next) {
+  res.render('lodash/string');
+});
+router.get('/util', function(req, res, next) {
+  res.render('lodash/util');
+});
+router.get('/properties', function(req, res, next) {
+  res.render('lodash/properties');
+});
+router.get('/methods', function(req, res, next) {
+  res.render('lodash/methods');
+});
 module.exports = router;
